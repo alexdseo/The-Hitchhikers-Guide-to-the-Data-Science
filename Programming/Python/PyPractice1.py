a035c8c1eb8ed516629c32093db0c57d3e92ca2a
deepdong <donghyunmon@gmail.com>
2020-09-26 21:22:39 -0500

    PyPractice

--- Programming/Python/PyPractice1.py
+++ Programming/Python/PyPractice1.py
@@ -2,7 +2,7 @@ marks = [90, 25, 67, 45, 80]
 
 number = 0
 for mark in marks:
-    number = number +1 
+    number = number +1
     if mark < 60:
         continue
-    print("%d번 학생 축하합니다. 합격입니다. " % number)
+    print("%d번 학생 축하합니다. " % number)
