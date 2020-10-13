0fddef88e299c56d2ae92cc5a3fb76019f9fe3f4
deepdong <donghyunmon@gmail.com>
2020-10-13 04:29:47 -0500

    PyPractice

--- Programming/Python/PyPractice1.py
+++
@@ -1,8 +0,0 @@
-marks = [90, 25, 66, 42, 80]
-
-number = 0
-for mark in marks:
-    number = number +1
-    if mark < 60:
-        continue
-    print("%d번 학생 축하합니다. " % number)
--- Programming/Python/PyPractice4.py
+++
@@ -1,9 +0,0 @@
-import re
-
-data = """
-park 800905-1025443
-kim  700905-1059119
-"""
-
-pat = re.compile("(\d{6})[-]\d{7}")
-print(pat.sub("\g<1>-*******", data))
