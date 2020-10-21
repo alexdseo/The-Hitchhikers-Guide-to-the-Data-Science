6384c220774fa56084c6fef9233c5765d50a1c61
deepdong <donghyunmon@gmail.com>
2020-10-21 04:35:58 -0500

    leetcode

--- Programming/Python/Leetcode/Two_sum.py
+++ Programming/Python/Leetcode/Two_sum.py
@@ -1,5 +1,16 @@
 # # Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to
 # target.
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    # @return a tuple, (index1, index2)
+    def twoSum(self, nums, target):
+        residual = {}
+        for i, n in enumerate(nums):
+            if n in residual:
+                return [residual[n], i]
+            else:
+                residual[target - n] = i
 
+
+num, target = [2, 11, 6, 7, 4], 9
+fun = Solution()
+print(fun.twoSum(num, target))
