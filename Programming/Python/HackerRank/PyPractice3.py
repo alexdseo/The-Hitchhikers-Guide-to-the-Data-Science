ff2a47f9db899b5ab4d843edbc2a5070c99ae16d
deepdong <donghyunmon@gmail.com>
2020-10-27 05:11:40 -0500

    programming

---
+++ Programming/Python/HackerRank/PyPractice3.py
@@ -0,0 +1,13 @@
+##11. Finding the Percentage
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+
+    print("{:.2f}".format(sum(student_marks[query_name])/len(student_marks[query_name])))
+
+## 12. Lists
