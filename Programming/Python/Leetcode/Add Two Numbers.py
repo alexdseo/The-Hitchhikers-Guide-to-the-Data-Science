6402161d51798212914431bec22ee332d4796d60
deepdong <donghyunmon@gmail.com>
2020-12-23 22:17:29 -0600

    py

--- Programming/Python/Leetcode/Add Two Numbers.py
+++ Programming/Python/Leetcode/Add Two Numbers.py
@@ -1,6 +1,6 @@
 # You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse
 # order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
-# Definition for singly-linked list
+# Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
